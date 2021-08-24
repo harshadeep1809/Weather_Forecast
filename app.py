d5f23ac9d6177c931af8fd7823f68300a2d04ea7
Harshadeep Nimmagadda <48246685+harshadeep1809@users.noreply.github.com>
2021-08-24 19:16:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ model = load_model(r'model.h5',compile=True)
 
 import keras
 import pandas as pd
-dd = pd.read_csv("https://raw.githubusercontent.comharshadeep1809/Time_Series_Weather_Forecast/main/ValFeatures.csv")
+dd = pd.read_csv("https://raw.githubusercontent.com/harshadeep1809/Time_Series_Weather_Forecast/main/ValFeatures.csv")
 y = pd.read_csv("https://raw.githubusercontent.com/harshadeep1809/Time_Series_Weather_Forecast/main/ValLabels.csv")
 import numpy as np
 batch_size = 256
